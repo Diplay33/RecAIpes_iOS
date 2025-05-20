ed9376213b72f0f051cb62ecf87d54ca8a54fc0c
Jacques HU <jacqueshu@Apple-MacBook-Pro-14-M4-de-Diplay.local>
2025-05-20 14:22:57 +0200

    initial commit

---
+++ RecAIpes_iOS/RecAIpes_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecAIpes_iOSApp.swift
+//  RecAIpes_iOS
+//
+//  Created by Jacques HU on 20/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct RecAIpes_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
