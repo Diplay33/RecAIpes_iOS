8fc2ff5479e808aeeaa82c409f58e2d505aa6a40
Jacques HU <jacqueshu@Apple-MacBook-Pro-14-M4-de-Diplay.local>
2025-06-10 14:28:02 +0200

    improve search behavior

--- RecAIpes_iOS/View/RecipeFiles/RecipeFilesList.swift
+++ RecAIpes_iOS/View/RecipeFiles/RecipeFilesList.swift
@@ -9,13 +9,40 @@ import SwiftUI
 
 struct RecipeFilesList: View {
     @State var searchText: String = ""
+    @State var searchResults: [RecipeFile] = []
     
     @Binding var recipeFiles: [RecipeFile]
     
     var body: some View {
         ScrollView {
-            ForEach(searchText.isEmpty ? recipeFiles : recipeFiles.filter { $0.title.lowercased().trimmingCharacters(in: .whitespacesAndNewlines).folding(options: .diacriticInsensitive, locale: .current).contains(searchText.lowercased().trimmingCharacters(in: .whitespacesAndNewlines).folding(options: .diacriticInsensitive, locale: .current)) }) { file in
-                RecipeFilesCell(file: file)
+            VStack(alignment: .leading, spacing: 5) {
+                if !searchText.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty && searchResults.isEmpty {
+                    ZStack {
+                        Text("Aucune recette trouvée...")
+                            .font(.title2)
+                            .fontWeight(.semibold)
+                            .multilineTextAlignment(.center)
+                    }
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 250)
+                    .background(.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 15))
+                    .padding(.horizontal)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+                    .shadow(color: Color.white.opacity(0.6), radius: 5, x: -4, y: -4)
+                    .shadow(color: Color(red: 0.6, green: 0.7, blue: 0.6).opacity(0.25), radius: 6, x: 4, y: 4)
+                }
+                else {
+                    Text(searchText.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty ? "Liste des recettes générées" : "Résultats pour \"\(searchText.trimmingCharacters(in: .whitespacesAndNewlines))\"")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .padding(.horizontal)
+                    
+                    ForEach(searchText.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty ? recipeFiles : searchResults) { file in
+                        RecipeFilesCell(file: file)
+                    }                    
+                }
             }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
@@ -24,6 +51,9 @@ struct RecipeFilesList: View {
             RecipeFiles.getAllRecipeFiles {
                 self.recipeFiles = $0
             }
+        }
+        .onChange(of: searchText) {
+            self.searchResults = recipeFiles.filter { $0.title.lowercased().trimmingCharacters(in: .whitespacesAndNewlines).folding(options: .diacriticInsensitive, locale: .current).contains(searchText.lowercased().trimmingCharacters(in: .whitespacesAndNewlines).folding(options: .diacriticInsensitive, locale: .current)) }
         }
     }
 }
